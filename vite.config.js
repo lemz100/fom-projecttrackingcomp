8ff3265b47b5d64bcf8950248dc5c691c250bd06
lemuel-100 <127439693+lemz100@users.noreply.github.com>
2025-10-14 21:06:47 +0100

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,28 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path';
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './tests/setup.js'
+  },
+  css: {
+  preprocessorOptions: {
+    less: {
+      javascriptEnabled: true,
+      additionalData: `@import "src/global.less";`
+    }
+  }
+}
+})
+
+
